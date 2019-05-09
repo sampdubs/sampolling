6f15fd3f02b255170b248000bc42322dbcf85108
Sam Prausnitz-Weinbaum <sammypw1@gmail.com>
2019-05-09 08:05:45 -0400

    Created basic skeleton for one question polls

--- main.py
+++ main.py
@@ -19,18 +19,26 @@ def index():
 def results(id):
     if request.method == 'POST':
         result = request.form
-        poll = {}
-        poll['question'] = result['question']
-        poll['choices'] = []
-        for name in result:
-            if 'answer' in name:
-                poll['choices'].append(result[name])
-        polls[id] = poll
-    return render_template('results.html')
+        if 'question' in result:
+            poll = {}
+            poll['question'] = result['question']
+            poll['choices'] = []
+            poll['answers'] = {}
+            poll['responses'] = 0.0
+            for name in result:
+                if 'answer' in name:
+                    if len(result[name]) > 0: 
+                        poll['choices'].append(result[name])
+                        poll['answers'][result[name]]  = 0
+            polls[id] = poll
+        else:
+            polls[id]['answers'][result['answer']] += 1.0
+            polls[id]['responses'] += 1
+    return render_template('results.html', poll=polls[id], round=round)
 
 @app.route("/poll/<id>/")
 def take(id):
-    return f"take {id}"
+    return render_template("answer.html", poll=polls[id], id=id)
 
 if __name__ == "__main__":
     print("Starting server...")
