914dd975a0ac1060c5085fdcca621cf5b6945b9d
Sam Prausnitz-Weinbaum <sammypw1@gmail.com>
2019-05-10 22:37:27 -0500

    fixed it so that multi-question with cookies works

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 from flask import Flask, url_for, redirect, render_template, make_response, request
-import json, datetime
+import json
+import datetime
 app = Flask(__name__)
 
 polls = {}
+
 
 @app.route("/poll/create/")
 def create():
     return render_template("create.html")
 
+
 @app.route("/poll/")
 def poll():
     return redirect(url_for('create'))
 
+
 @app.route("/")
 def index():
     return redirect(url_for('create'))
+
 
 @app.route("/poll/<id>/results/", methods=['POST', 'GET'])
 def results(id):
@@ -33,7 +38,7 @@ def results(id):
                     tpoll['responses'] = 0.0
                     for name in result:
                         if f'answer{i}_' in name:
-                            if len(result[name]) > 0: 
+                            if len(result[name]) > 0:
                                 tpoll['choices'].append(result[name])
                                 tpoll['answers'][result[name]] = 0
                     poll.append(tpoll)
@@ -41,47 +46,55 @@ def results(id):
                     break
             polls[id] = poll
         else:
-            polls[id][0]['answers'][result['answer'].replace("****SPACE****", " ")] += 1
-            polls[id][0]['responses'] += 1
+            polls[id][0]['answers'][result['answer'].replace(
+                "****SPACE****", " ")] += 1
+            polls[id][0]['responses'] += 1.0
     return render_template('results.html', poll=polls[id], round=round)
+
 
 @app.route("/poll/<id>/")
 def take_redirect(id):
     return redirect(request.path + "1/")
 
+
 @app.route("/poll/<id>/<qnumber>/", methods=['POST', 'GET'])
 def take(id, qnumber):
-    qnumber = int(qnumber) - 1
-    if  request.method == 'POST':
+    qnumber = int(qnumber) - 2
+    if request.method == 'POST':
         result = request.form
-        polls[id][qnumber - 1]['answers'][result['answer'].replace("****SPACE****", " ")] += 1
-        polls[id][qnumber - 1]['responses'] += 1
-        if qnumber >= len(polls[id]):
-            return redirect(url_for('results', id=id))
+        polls[id][qnumber]['answers'][result['answer'].replace(
+            "****SPACE****", " ")] += 1
+        polls[id][qnumber]['responses'] += 1.0
+    qnumber += 1
+    if qnumber >= len(polls[id]):
+        return redirect(url_for('results', id=id))
     cook = request.cookies.get('taken')
     if cook != None:
         cook = json.loads(cook)
     else:
         cook = {}
     if id in cook:
-        taken = cook[id]
+        taken = cook[id][str(qnumber + 1)]
     else:
         taken = False
         cook[id] = {}
         for i in range(len(polls[id])):
             cook[id][str(i + 1)] = False
-        cook[id][qnumber] = False
-        resp = make_response(render_template("answer.html", poll=polls[id][qnumber], id=id))
-        resp.set_cookie('taken', json.dumps(cook), expires=(datetime.datetime.now()+datetime.timedelta(days=10)))
+        cook[id][str(qnumber + 1)] = True
+        resp = make_response(render_template(
+            "answer.html", poll=polls[id][qnumber], id=id))
+        resp.set_cookie('taken', json.dumps(cook), expires=(
+            datetime.datetime.now()+datetime.timedelta(days=10)))
         return resp
     if taken:
         return 'You have already answered this question'
-    for i in range(len(polls[id])):
-        cook[id][str(i + 1)] = False
-    cook[id][qnumber] = True
-    resp = make_response(render_template("answer.html", poll=polls[id][qnumber], id=id))
-    resp.set_cookie('taken', json.dumps(cook), expires=(datetime.datetime.now()+datetime.timedelta(days=10)))
+    cook[id][str(qnumber + 1)] = True
+    resp = make_response(render_template(
+        "answer.html", poll=polls[id][qnumber], id=id))
+    resp.set_cookie('taken', json.dumps(cook), expires=(
+        datetime.datetime.now()+datetime.timedelta(days=10)))
     return resp
+
 
 if __name__ == "__main__":
     print("Starting server...")
